ca871c82f203b35ca324359c11fcc1f13a1ab220
Admin <Admin@TEB127025>
2022-10-28 11:11:10 +0200

    Add project files.

---
+++ uppgift4.26/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift426
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal, talen måste vara heltal.");
+            string tal =Console.ReadLine();
+
+            for (int i =0; i < tal.Length; i++)
+            {
+                string tecken = tal[i].ToString();
+                int siffra = int.Parse(tecken);
+                int tal2 = siffra + 1;
+                if (tal2 == 10)
+                {
+                    tal2 = 0;
+                }
+                Console.Write(tal2);
+            }
+            Console.ReadKey();
+        }
+    }
+}
